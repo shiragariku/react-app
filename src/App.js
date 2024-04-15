78751c777bf71a1a9eada6b85047bb09b3ead6d1
shiragariku <r.shiraga@senkyo.co>
2024-04-16 02:57:32 +0900

    TodoListコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState();
+
+
   return (
-    <div className="App">hello</div>
+    <>
+      <TodoList/>
+      <input type="text"/>
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
 
