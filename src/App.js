0d7a6a42d1626478a600a65436a297b21af32aba
shiragariku <r.shiraga@senkyo.co>
2024-04-18 03:13:29 +0900

    todo追加処理作成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState([
+    {id:1, name:"Todo1", completed:true},
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id:1, name, completed:false}]
+    })
+    todoNameRef.current.value = null;
+  };
 
 
   return (
     <>
-      <TodoList/>
-      <input type="text"/>
-      <button>タスクを追加</button>
+      <TodoList todos={todos}/>
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク:0</div>
     </>
